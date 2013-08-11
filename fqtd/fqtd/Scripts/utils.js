eaf86d92148d3ff6fa9367f83decc3dff848ea4c
lam truong phong <phonglam29305@gmail.com>
2013-08-11 17:46:47 +0700

    update admin layout
    con hoi xau hehe

--- fqtd/fqtd/Scripts/utils.js
+++ fqtd/fqtd/Scripts/utils.js
@@ -195,8 +195,8 @@ var FQTD = (function () {
 
         },
         noRecord: function () {
-            $("#list").html("<p style='text-align:center'>Thông tin tìm kiếm hiện chưa cập nhật. Vui lòng tìm lại sau.</p>");
-            $("#map").html("<p style='text-align:center'>Thông tin tìm kiếm hiện chưa cập nhật. Vui lòng tìm lại sau.</p>");
+            $("#list").html("<p style='text-align:center'>Thông tin Search hiện chưa cập nhật. Vui lòng tìm lại sau.</p>");
+            $("#map").html("<p style='text-align:center'>Thông tin Search hiện chưa cập nhật. Vui lòng tìm lại sau.</p>");
             FQTD.displayMap();
         },
         yesRecord: function () {
