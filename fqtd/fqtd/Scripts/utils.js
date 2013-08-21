4c555721c50505c9d015debbb3b169aca8219e76
Phạm Dương Anh Vũ <anhvuphamx@yahoo.com>
2013-08-21 11:38:34 +0700

    Fix bug submit advance search

--- fqtd/fqtd/Scripts/utils.js
+++ fqtd/fqtd/Scripts/utils.js
@@ -713,7 +713,7 @@ var FQTD = (function () {
                     var data = '?id=' + categoryVal;
                     $("#brand").empty();
                     var result = $.getJSON(siteurl + data, null, function (brands) {
-                        $("#brand").append('<option value="">Tất cả</option>');
+                        $("#brand").append('<option value="-1">Tất cả</option>');
                         for (i in brands) {
                             brand = brands[i];
                             $("#brand").append('<option value="' + brand.BrandID + '">' + brand.BrandName + '</option>');;
