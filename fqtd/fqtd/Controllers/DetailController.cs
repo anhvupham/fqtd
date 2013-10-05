bc2901871c38ead7f121e2c5484db3f7463acc63
lam truong phong <phonglam29305@gmail.com>
2013-10-06 01:31:20 +0700

    add item viewbag

--- fqtd/fqtd/Controllers/DetailController.cs
+++ fqtd/fqtd/Controllers/DetailController.cs
@@ -13,7 +13,13 @@ namespace fqtd.Controllers
         {
             ViewBag.URL = Request.Url.AbsoluteUri;
             ViewBag.keywords = ConfigurationManager.AppSettings["metakeywords"];
-            ViewBag.description = ConfigurationManager.AppSettings["metakeydescription"];            
+            ViewBag.description = ConfigurationManager.AppSettings["metakeydescription"];
+            string temp = Request.Url.AbsoluteUri.Substring(Request.Url.AbsoluteUri.IndexOf("detail/")+7);
+            temp = temp.Substring(0,temp.IndexOf("/"));
+            int itemid = 0;
+            int.TryParse(temp, out itemid);
+            ViewBag.Item = new fqtd.Areas.Admin.Models.fqtdEntities().BrandItems.Find(itemid);
+
             return View("Index");
             
         }      
