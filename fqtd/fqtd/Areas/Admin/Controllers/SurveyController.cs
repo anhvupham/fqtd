e635d99f1d38388acd23c5820093930e90d3a7f3
lam truong phong <phonglam29305@gmail.com>
2013-10-27 23:03:03 +0700

    add survey

---
+++ fqtd/fqtd/Areas/Admin/Controllers/SurveyController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fqtd.Areas.Admin.Models;
+
+namespace fqtd.Areas.Admin.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private fqtdEntities db = new fqtdEntities();
+
+        //
+        // GET: /Admin/Survey/
+
+        public ActionResult Index()
+        {
+            return View(db.Survey.ToList());
+        }
+
+        //
+        // GET: /Admin/Survey/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Survey survey = db.Survey.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        //
+        // GET: /Admin/Survey/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/Survey/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Survey.Add(survey);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(survey);
+        }
+
+        //
+        // GET: /Admin/Survey/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Survey survey = db.Survey.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        //
+        // POST: /Admin/Survey/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(survey).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(survey);
+        }
+
+        //
+        // GET: /Admin/Survey/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Survey survey = db.Survey.Find(id);
+            if (survey == null)
+            {
+                return HttpNotFound();
+            }
+            return View(survey);
+        }
+
+        //
+        // POST: /Admin/Survey/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Survey survey = db.Survey.Find(id);
+            db.Survey.Remove(survey);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
